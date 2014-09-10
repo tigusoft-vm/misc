900a38b5b376bc73243137cfe80cbde0a8929796
rfree2monero <rfreemonero@op.pl>
2014-09-10 17:10:29 +0200

    more work on size_t for comparsions, and to unsign

--- coding-standard/size_t_sign/a.cpp
+++ coding-standard/size_t_sign/a.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <stdexcept>
 
 #define ALLOWED_BOOST 1 // XXX
 
 #ifdef ALLOWED_BOOST
 	#include <boost/integer.hpp>
-	typedef boost::int_t< sizeof(size_t)*8 +1 >::fast msize_t; // the full version of size_t but with sign
+	typedef boost::int_t< 128 >::fast msize_t; 
+	// the full version of size_t but with sign
 #else
 	typedef signed long long int msize_t;
 	// TODO static assert that this type can handle minus MAX_SIZE too
@@ -30,14 +34,49 @@ void melt(const vector<string> &objects, int t) { // t = room temperature
 	cout << ";" << endl;
 }
 
-int main() {
-	cout << " --- START --- " << endl;
-
+void main1() {
 	vector<string> objects = { "snow", "icecube", "hard-ice", "iced-gell", "gell"};
-
 	melt( objects , -10); // nothing melts
 	melt( objects , 2); // snow and icecube melt
 	melt( objects , 10); // everything melted
+}
+
+template <class T>
+size_t make_size_t(T val) {
+	if (val<0) {
+		ostringstream oss; oss<<"Negative size_t value attempted: " << val; 
+		throw std::runtime_error(oss.str());
+	}
+	cout << SIZE_MAX << endl;
+	cout << (msize_t)SIZE_MAX << endl;
+	if (val > (msize_t)SIZE_MAX) {
+		ostringstream oss; oss<<"Overflow size_t value attempted: " << val; 
+		throw std::runtime_error(oss.str());
+	}
+	return val;
+}
+
+void func( size_t ix ) {
+	vector<int> tab;
+	if (false) tab.at(ix);
+}
+
+void main2() {
+	{
+		int temp = 15;
+		func( make_size_t(temp) );
+	}
 	
+	{
+		long long int temp = 999999999999LL;
+		func( make_size_t(temp) );	
+	}
+}
+
+int main() {
+	cout << " --- START --- " << endl;
+	cout << "size=" << sizeof( size_t ) << endl;
+	main1();
+	main2();
 }
 
